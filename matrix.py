3a36f35cae02ea50c346d075595924fab7f8e827
danielsela42 <danielsela42@gmail.com>
2021-10-29 10:26:25 -0500

    Example run and fixed slight bug

--- matrix.py
+++ matrix.py
@@ -19,6 +19,22 @@ def get_gvects(g_file):
             g_vects.append(np.array([float(values.group('f1')), float(values.group('f2'))]))
     return g_vects
 
+def reorder(gvects1, gvects2,error):
+    ''' Reorder g_vects1 to the order of gvects2 
+    '''
+    gvects = list()
+
+    for vect in gvects2:
+        count = 0
+        for vect1 in gvects1:
+            if abs(vect1[0] - vect[0]) < error and abs(vect1[1] - vect[1]) < error:
+                print(vect1, vect)
+                gvects.append(vect1)
+                count += 1
+        if count != 1: raise Exception("ERROR: More than one matching vector")
+    
+    return gvects
+
 def construct_matrix(n_g, g_vects, neigh_distances, off_elem, factor, k, error):
     ''' construct Hermitian matrix
 
@@ -47,7 +63,7 @@ def construct_matrix(n_g, g_vects, neigh_distances, off_elem, factor, k, error):
 
                     neigh_distance = neigh_distances[0]
                     elem = complex(0., 0.)
-                    if abs(diff_mag, neigh_distance) < error:
+                    if abs(diff_mag - neigh_distance) < error:
                         if row > col: elem = off_elem
                         else: elem = off_elem.conjugate()
                 g1_row.append(elem)
@@ -55,7 +71,8 @@ def construct_matrix(n_g, g_vects, neigh_distances, off_elem, factor, k, error):
     return np.array(matrix_list)
 
 
-def eigens(gfile, phase):
+def eigens(matrix):
+    '''
     n_g = 127
     V = 33.5
     g_vects = get_gvects(gfile) # These are read from the file
@@ -66,6 +83,7 @@ def eigens(gfile, phase):
     error = 10**(-8)
 
     matrix = construct_matrix(n_g, g_vects, neigh_distances, off_elem ,factor, k, error)
+    '''
 
     # Calculate eigenvalues and vectors and perform checks
     eigvals_prev, eigvecs = np.linalg.eigh(matrix)
@@ -84,3 +102,29 @@ def eigens(gfile, phase):
     k_eigen_list = [(eigvals[i], eigvecs[:, i]) for i in range(n_g)]
     k_eigen_list.sort(key=lambda tup: tup[0])
     return k_eigen_list
+
+if __name__=='__main__':
+    gfile1 = "/home/dsela/ut_projects/cmp/code_eigenvalue/Gbasis_127_1.txt"
+    gfile2 = "/home/dsela/ut_projects/cmp/code_eigenvalue/Gbasis_127_2.txt"
+
+    phase = 120
+
+    n_g = 127
+    V = 33.5
+    neigh_distances = [1]
+    off_elem = V*np.exp(complex(0, phase*np.pi/180))
+    factor = -8.401847020709548
+    k = np.array([0, 0])
+    error = 10**(-8)
+
+    g_vects1_prev = get_gvects(gfile1)
+    g_vects2 = get_gvects(gfile1)
+    g_vects1 = reorder(g_vects1_prev, g_vects2, error)
+
+    matrix1 = construct_matrix(n_g, g_vects1, neigh_distances, off_elem, factor, k, error)
+    matrix2 = construct_matrix(n_g, g_vects1, neigh_distances, off_elem, factor, k, error)
+
+    # Compare matrix component-wise
+    for row in range(n_g):
+        for col in range(n_g):
+            print(matrix1[row, col], matrix2[row,col])
